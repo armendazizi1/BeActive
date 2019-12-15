42bd4e2220bc3fa0cdb9be8705005e3d162e66a6
Simone <Masiero>
2019-12-15 14:29:41 +0100

    added badge system

--- app/src/main/java/com/example/beactive/StatsActivity.java
+++ app/src/main/java/com/example/beactive/StatsActivity.java
@@ -1,15 +1,57 @@
 package com.example.beactive;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class StatsActivity extends AppCompatActivity {
+public class StatsActivity extends AppCompatActivity implements SensorEventListener {
+
+    private String sharedPrefFile = "com.example.StepCounter";
+    SensorManager sensorManager;
+    boolean running = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+
+        } else {
+            Toast.makeText(this, "Sensor not found", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
+
+
+        //comment this if you dont want to stop listening for step counts
+        // sensorManager.unregisterListener(this);
+
     }
 
     @Override
@@ -17,6 +59,20 @@ public class StatsActivity extends AppCompatActivity {
 
         startActivity(new Intent(StatsActivity.this,MainActivity.class));
         finish();
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int b =  (int) event.values[0];
+        if (b > 80){
+            ImageView goldenBridge = findViewById(R.id.GBridge);
+            goldenBridge.setImageResource(R.drawable.bridge_unlock);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 }
