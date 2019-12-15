112323dc2854d111ac2c1a4bb5829b1327d19e04
Simone <Masiero>
2019-12-15 10:58:28 +0100

    done

--- app/src/main/java/com/example/beactive/MainActivity.java
+++ app/src/main/java/com/example/beactive/MainActivity.java
@@ -87,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
                 // Set up the input
                 final EditText input = new EditText(cn);
                 // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
                 builder.setView(input);
 
                 // Set up the buttons
@@ -191,7 +191,7 @@ public class MainActivity extends AppCompatActivity {
              // Set up the input
             final EditText input = new EditText(cn);
             // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-            input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            input.setInputType(InputType.TYPE_CLASS_TEXT);
             builder.setView(input);
 
             // Set up the buttons
