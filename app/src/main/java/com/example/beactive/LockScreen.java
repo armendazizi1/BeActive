2deaa4f7316124dfac89aaf9b5944e1a8f0d0be3
armend <armend.azizi@usi.ch>
2019-12-17 20:04:33 +0100

    incredible bug fix (heart eyes emoji)

--- app/src/main/java/com/example/beactive/LockScreen.java
+++ app/src/main/java/com/example/beactive/LockScreen.java
@@ -168,4 +168,15 @@ public class LockScreen extends AppCompatActivity implements SensorEventListener
 
     }
 
+    // Launch Home Screen after pressing the back button
+    @Override
+    public void onBackPressed() {
+
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
+
+    }
+
 }
