1de7f0f6e0cefac50676416197c5f70470f8395d
Simone <Masiero>
2019-12-17 17:46:02 +0100

    data on badge section

--- app/src/main/java/com/example/beactive/BadgeActivity.java
+++ app/src/main/java/com/example/beactive/BadgeActivity.java
@@ -10,6 +10,7 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class BadgeActivity extends AppCompatActivity implements SensorEventListener {
@@ -17,15 +18,28 @@ public class BadgeActivity extends AppCompatActivity implements SensorEventListe
     private String sharedPrefFile = "com.example.StepCounter";
     SensorManager sensorManager;
     boolean running = false;
+    private float total_steps = 100;
+    TextView tot_steps;
+    TextView tot_distance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);     //  Fixed Portrait orientation
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_badge);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        tot_steps = findViewById(R.id.total_steps);
+        tot_distance = findViewById(R.id.Total_distance);
+
+        tot_steps.setText("Total number of steps: " + (int)total_steps);
+        tot_distance.setText("Distance travelled so far: " +(int)(total_steps*1.6) +" m");
+
     }
 
     @Override
@@ -66,6 +80,7 @@ public class BadgeActivity extends AppCompatActivity implements SensorEventListe
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+//        this.total_steps = event.values[0];
         int b = (int) event.values[0];
         if (b > 80) {
             ImageView goldenBridge = findViewById(R.id.GBridge);
